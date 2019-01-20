ad377758aad0408ec694645fcd003940dd143db9
Yizhan Wu <39906408+Metalheadache@users.noreply.github.com>
2019-01-20 21:37:32 +0800

    update

--- QQbot/functions/plugins/WFalerts.py
+++ QQbot/functions/plugins/WFalerts.py
@@ -28,6 +28,14 @@ def loadDict():
     file.close()
     return itemDirc
 
+# 载入紫卡词库
+def loadRiven():
+    path = os.path.dirname(os.path.realpath(__file__))
+    rivenDict = {}
+    with open(path + "\data_riven.json", "r", encoding='UTF-8') as F:
+        rivenDict["\data_riven.json"] = json.loads(F.read())
+    return rivenDict
+
 # 获取JSON
 def updateJson():
     url = 'http://content.warframe.com/dynamic/worldState.php'
@@ -92,11 +100,11 @@ def TimeConversion(seconds):
 	m, s = divmod(seconds, 60)
 	h, m = divmod(m, 60)
 	if d > 0:
-		return '%d天%02d:%02d:%02d' % (d, h, m, s)
+		return '%d 天 %02d 时 %02d 分 %02d 秒' % (d, h, m, s)
 	if h > 0:
-		return '%d:%02d:%02d' % (h, m, s)
+		return '%d 时 %02d 分 %02d 秒' % (h, m, s)
 	else:
-		return '%02d:%02d' % (m, s)
+		return '%02d 分 %02d 秒' % (m, s)
 
 @on_command('sortie', aliases=('突击', '每日'))
 # 突击
@@ -171,14 +179,14 @@ async def get_voidtrader(session: CommandSession):
     output = ''
     if float(worldData['VoidTraders'][0]['Activation']['$date']['$numberLong'])/1000 - time.time() < 0 and float(worldData['VoidTraders'][0]['Expiry']['$date']['$numberLong'])/1000 - time.time() > 0:
         inventory = worldData['VoidTraders'][0]['Manifest']
-        output = '奸商已到达{}，还有{}就要离开了。\n本次携带的物品是：'.format(worldDict[0][worldData['VoidTraders'][0]['Node']], TimeConversion(float(worldData['VoidTraders'][0]['Expiry']['$date']['$numberLong'])/1000 - time.time()))
+        output = '奸商已到达 {}，还有 {} 就要离开了。\n本次携带的物品是：'.format(worldDict[0][worldData['VoidTraders'][0]['Node']], TimeConversion(float(worldData['VoidTraders'][0]['Expiry']['$date']['$numberLong'])/1000 - time.time()))
         for item in inventory:
             try:
                 output = output + '\n物品：{} 虚空币：{} 现金：{}'.format(worldDict[0][item['ItemType']], item['PrimePrice'], item['RegularPrice'])
             except:
                 output = output + '\n物品：{} 虚空币：{} 现金：{}'.format(item['ItemType'], item['PrimePrice'], item['RegularPrice'])
     else:
-        output = '奸商还在来的路上，将于{}后到达{}。'.format(TimeConversion(float(worldData['VoidTraders'][0]['Activation']['$date']['$numberLong'])/1000 - time.time()), worldDict[0][worldData['VoidTraders'][0]['Node']])
+        output = '奸商还在路上，将于 {} 后到达 {}。'.format(TimeConversion(float(worldData['VoidTraders'][0]['Activation']['$date']['$numberLong'])/1000 - time.time()), worldDict[0][worldData['VoidTraders'][0]['Node']])
     await session.send(output)
 
 
@@ -203,7 +211,7 @@ async def CetusTime(session: CommandSession):
     await session.send(output)
 
 @on_command('Fortuna', aliases=('平原天气', '金星平原'))
-# 金星平原时间
+# 金星平原天气
 async def FortunaTime(session: CommandSession):
 	weather_cycle = ''
 	# 1541837628 and a 36 sec delay
@@ -264,6 +272,7 @@ async def Invasions(session: CommandSession):
     await session.send(output)
 
 @on_command('fissures', aliases=('裂缝', '裂隙'))
+# 裂缝
 async def Fissures(session: CommandSession):
     updateJson()
     output = '现在的虚空裂缝有：\n'
@@ -310,6 +319,158 @@ async def Fissures(session: CommandSession):
             seconds = int(eta_finish - minute*60 - hour*3600)
         output = output + '\n后纪(T4)：' + worldDict[0][i['Node']] + ' | ' + worldDict[0][i['MissionType']]
         output = output + '\n时限：' + str(hour) + ' 小时 ' + str(minute) + ' 分钟 ' + str(seconds) + ' 秒 \n'
+    await session.send(output)
+
+# 紫卡模块
+# 紫卡数据
+rivenDict = loadRiven()
+riven_type = {
+	'Melee': '近战',
+	'Rifle': '步枪',
+	'Pistol': '手枪',
+	'Shotgun': '霰弹枪',
+	'Zaw': 'Zaw',
+	'Kitgun': 'Kitgun'
+}
+riven_data = {}
+riven_weapons = {}
+for k in riven_type:
+    riven_data[k] = {
+        'dispo': {},
+        'buff': {},
+        'prefix': {},
+        'suffix': {},
+        'curse': {}
+    }
+    for weapon in rivenDict["\data_riven.json"][k]['Rivens']:
+        riven_data[k]['dispo'][weapon['name']] = weapon['disposition']
+        riven_weapons[weapon['name']] = k
+    for buff in rivenDict["\data_riven.json"][k]['Buffs']:
+        riven_data[k]['buff'][buff['text']] = buff['value']
+        riven_data[k]['prefix'][buff['text']] = buff['prefix']
+        riven_data[k]['suffix'][buff['text']] = buff['suffix']
+    for curse in rivenDict["\data_riven.json"][k]['Buffs']:
+        if 'curse' in curse:
+            riven_data[k]['curse'][curse['text']] = curse['value']
+
+def riven_details(weapon, buffs, has_curse, simulate=0):
+    if weapon in riven_weapons:
+        curr_dispo = riven_data[riven_weapons[weapon]]['dispo']
+        curr_buff = riven_data[riven_weapons[weapon]]['buff']
+        curr_curse = riven_data[riven_weapons[weapon]]['curse']
+        curr_prefix = riven_data[riven_weapons[weapon]]['prefix']
+        curr_suffix = riven_data[riven_weapons[weapon]]['suffix']
+    else:
+        return ''
+    riven_info = ''
+    rand_coh = [0, 0, 0, 0]
+    for i in range(0, 3):
+        rand_coh[i] = random.random()*0.2+0.9
+    rand_coh.sort(reverse=True)
+    rand_coh[3] = random.random()*0.2+0.9
+    if buffs == 2:
+        if has_curse:
+            dispo = curr_dispo[weapon] * 1.25
+            dispo = dispo * 0.66 * 1.5
+            buffs = random.sample(list(curr_buff), 2)
+            temp_curr_curse = list(curr_curse)
+            for buff in buffs:
+                try:
+                    temp_curr_curse.remove(buff)
+                except:
+                    pass
+            curse = random.sample(temp_curr_curse, 1)
+            riven_info = weapon + ' ' + curr_prefix[buffs[0]] + curr_suffix[buffs[1]].lower() + '\n' + riven_dispo_icon(curr_dispo[weapon]) + '\n' \
+                + buffs[0].replace('|val|', str(round(rand_coh[0]*curr_buff[buffs[0]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[0] - 1)*100, 2)) + ']'\
+                + '\n' + buffs[1].replace('|val|', str(round(rand_coh[1]*curr_buff[buffs[1]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[1] - 1)*100, 2)) + ']'\
+                + '\n' + curse[0].replace('|val|', str(-1*round(rand_coh[2]*curr_curse[curse[0]]*curr_dispo[weapon]
+                                                                * 1.5*0.33, 2))) + ' [' + riven_rank(round((rand_coh[2] - 1)*100, 2)) + ']'
+        else:
+            dispo = curr_dispo[weapon]  # 裂罅倾向
+            dispo = dispo * 0.66 * 1.5  # 2buff，无负，按照0.66，1.5紫卡系数
+            buffs = random.sample(list(curr_buff), 2)  # 下限系数0.9，上限系数1.1
+            riven_info = weapon + ' ' + curr_prefix[buffs[0]] + curr_suffix[buffs[1]].lower() + '\n' + riven_dispo_icon(curr_dispo[weapon]) + '\n' \
+                + buffs[0].replace('|val|', str(round(rand_coh[0]*curr_buff[buffs[0]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[0] - 1)*100, 2)) + ']' \
+                + '\n' + buffs[1].replace('|val|', str(round(rand_coh[1]*curr_buff[buffs[1]]*dispo, 2))
+                                            ) + ' [' + riven_rank(round((rand_coh[1] - 1)*100, 2)) + ']'
+    elif buffs == 3:
+        if has_curse:
+            dispo = curr_dispo[weapon] * 1.25
+            dispo = dispo * 0.5 * 1.5
+            buffs = random.sample(list(curr_buff), 3)
+            temp_curr_curse = list(curr_curse)
+            for buff in buffs:
+                try:
+                    temp_curr_curse.remove(buff)
+                except:
+                    pass
+            curse = random.sample(temp_curr_curse, 1)
+            riven_info = weapon + ' ' + curr_prefix[buffs[0]] + '-' + curr_prefix[buffs[1]].lower() + curr_suffix[buffs[2]].lower() + '\n' + riven_dispo_icon(curr_dispo[weapon]) + '\n' \
+                + buffs[0].replace('|val|', str(round(rand_coh[0]*curr_buff[buffs[0]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[0] - 1)*100, 2)) + ']' \
+                + '\n' + buffs[1].replace('|val|', str(round(rand_coh[1]*curr_buff[buffs[1]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[1] - 1)*100, 2)) + ']' \
+                + '\n' + buffs[2].replace('|val|', str(round(rand_coh[2]*curr_buff[buffs[2]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[2] - 1)*100, 2)) + ']' \
+                + '\n' + curse[0].replace('|val|', str(-1*round(rand_coh[3]*curr_curse[curse[0]]*curr_dispo[weapon]
+                                                                * 1.5*0.5, 2))) + ' [' + riven_rank(round((rand_coh[3] - 1)*100, 2)) + ']'
+        else:
+            dispo = curr_dispo[weapon]
+            dispo = dispo * 0.5 * 1.5
+            buffs = random.sample(list(curr_buff), 3)
+            riven_info = weapon + ' ' + curr_prefix[buffs[0]] + '-' + curr_prefix[buffs[1]].lower() + curr_suffix[buffs[2]].lower() + '\n' + riven_dispo_icon(curr_dispo[weapon]) + '\n' \
+                + buffs[0].replace('|val|', str(round(rand_coh[0]*curr_buff[buffs[0]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[0] - 1)*100, 2)) + ']' \
+                + '\n' + buffs[1].replace('|val|', str(round(rand_coh[1]*curr_buff[buffs[1]]*dispo, 2))) + ' [' + riven_rank(round((rand_coh[1] - 1)*100, 2)) + ']' \
+                + '\n' + buffs[2].replace('|val|', str(round(rand_coh[2]*curr_buff[buffs[2]]*dispo, 2))
+                                            ) + ' [' + riven_rank(round((rand_coh[2] - 1)*100, 2)) + ']'
+    return riven_info
+
+def riven_rank(perc):
+	rank = ''
+	if 9.5 < perc <= 10:
+		rank = 'SSS'
+	elif 9 < perc <= 9.5:
+		rank = 'SS'
+	elif 6 < perc <= 9:
+		rank = 'S'
+	elif 2 < perc <= 6:
+		rank = 'A'
+	elif -2 < perc <= 2:
+		rank = 'B'
+	elif -6 < perc <= -2:
+		rank = 'C'
+	elif -10 <= perc <= -6:
+		rank = 'D'
+	else:
+		rank = '?'
+	return rank
+
+def riven_dispo_icon(dispo):
+	text = ''
+	if dispo > 1.3:
+		text = '●●●●●'
+	elif 1.1 < dispo <= 1.3:
+		text = '●●●●○'
+	elif 0.9 <= dispo <= 1.1:
+		text = '●●●○○'
+	elif 0.7 <= dispo < 0.9:
+		text = '●●○○○'
+	elif dispo < 0.7:
+		text = '●○○○○'
+	else:
+		text = '?'
+	return ('%s(%.2f)' % (text, dispo))
+
+@on_command('getriven', aliases=('模拟开卡', '模拟开紫卡'))
+# 紫卡函数
+async def get_riven_info(session: CommandSession):
+    riven_info = ''
+    prefix = ''
+    weapon = random.choices(
+        population = ['Melee', 'Rifle', 'Pistol', 'Shotgun', 'Zaw', 'Kitgun'],
+        weights = [8.14, 6.79, 7.61, 1.36, 2.0, 2.0],
+        k=1
+    ).pop()
+    prefix = '你放弃了今日的传核，获得了一张%s裂罅Mod并开出了：\n' % (riven_type[weapon])
+    riven_info = riven_details(random.sample(list(riven_data[weapon]['dispo']), 1)[0], random.randint(2, 3), random.randint(0, 1))
+    output = prefix + riven_info
     await session.send(output)
 
 worldDict = loadDict()
